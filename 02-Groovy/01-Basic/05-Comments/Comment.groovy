2688b3072edf5822d85e6a0577d78fdb85b74488
root <root@master.example.com>
2023-11-27 15:05:11 +0000

    Jenkins

---
+++ 02-Groovy/01-Basic/05-Comments/Comment.groovy
@@ -0,0 +1,20 @@
+// this is a single line comments
+
+def msg = 'Hello Comment' // this is a message
+
+/*
+This is
+a multiline comment
+*/
+
+/**
+ * This is a person class
+ *
+ *
+ */
+class person {
+
+}
+
+
+
