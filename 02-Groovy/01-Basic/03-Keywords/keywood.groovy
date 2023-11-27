9798d1af953a3c98e2aa2d58d22f341481c6af8c
root <root@master.example.com>
2023-11-27 14:55:21 +0000

    Groovy

---
+++ 02-Groovy/01-Basic/03-Keywords/keywood.groovy
@@ -0,0 +1,11 @@
+/**
+ * My Applicaiton Class
+ */
+class class {
+
+    private final String NAME = "THE_REAL_Name_Amit"
+
+    String def = "some message"
+
+}
+
---
+++ 02-Groovy/08-Classes/app.groovy
@@ -0,0 +1,13 @@
+// create a new instance of a developer
+Developer d = new Developer()
+d.first = "Amit"
+d.setLast("Vashist")
+
+// assign some languages
+d.languages << "Groovy"
+d.languages << "Java"
+
+// call some methods
+d.work()
+
+
