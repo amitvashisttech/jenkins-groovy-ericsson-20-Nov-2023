f10dac752741a060631fe57997d73c555b3cbce6
root <root@master.example.com>
2023-11-28 13:54:29 +0000

    Groovy - 12-Grape

---
+++ 02-Groovy/02-Data-Types/01-Data-Types/app.groovy
@@ -0,0 +1,22 @@
+// In Groovy, eveything is an object
+def byte b = 10 
+println b 
+println b.class
+
+def short s = 1000
+println s.class
+
+
+def float f = 1.25 
+println f.class
+
+def a = 3.class
+println a 
+
+def b = 1.25
+println b
+println b.class
+
+def c = "Amit"
+println c 
+println c.class
